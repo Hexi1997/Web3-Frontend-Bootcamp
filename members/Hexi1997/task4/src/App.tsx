c4aeb7a1118a13623c0e6490f52a26be3f224092
Hexi1997 <18715156450@163.com>
2024-06-17 23:30:50 +0800

    feat: init task4 project

---
+++ members/Hexi1997/task4/src/App.tsx
@@ -0,0 +1,16 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+function App() {
+  return (
+    <>
+      <header className='flex justify-end'>
+        <ConnectButton />
+      </header>
+      <main>
+        app
+      </main>
+    </>
+
+  )
+}
+
+export default App
